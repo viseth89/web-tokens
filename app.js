44cf45de91ca00423cd996f3a3d71367a3002e47
viseth89 <viseth89@gmail.com>
2021-09-06 17:07:26 -0500

    13 Minute Mark, digesting how the verifyToken function works to cause the Forbidden

--- app.js
+++ app.js
@@ -10,26 +10,47 @@ app.get('/api', (req, res) => {
 });
 
 
-app.post('/api/posts',(req, res) => {
+app.post('/api/posts', verifyToken,(req, res) => {
     res.json({
         message: 'Post Created'
     })
 });
 
 
-app.post('/api/login', (req, res) => {
+app.post('/api/login',  (req, res) => {
     // Mock user
     const user = {
         id:1,
         username: 'viseth',
         email:'viseth89@gmail.com'
     }
-    
 
-    jwt.sign();
+
+    jwt.sign({ user: user}, 'secretkey', (err, token) => {
+        res.json({
+            token
+        });
+    });
 });
 
+// FORMAT OF TOKEN
+// Authorization: Bearer <access_token>
 
+
+// Verify Token
+
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    // Check if bearer is undefined
+    if(typeof bearerHeader !== 'undefined'){
+        
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+
+}
 
 app.listen(5000, () => console.log('Server Started on port 5000'))
 
